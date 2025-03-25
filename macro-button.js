295b92f9271d5d79b11be9e67944765a6d8cb83f
spydyrsbyte <spydyrs.byte@gmail.com>
2025-03-25 12:34:59 -0500

    Update macro-button.js

--- macro-button.js
+++ macro-button.js
@@ -1,7 +1,20 @@
-Hooks.on("renderChatMessage", (message,element,info) => {
-	console.log(element('[data-type="macro.button"]'));
-	 
-	
-	
+function macro_run (event){
+    const element = event.target;
+    const macroName = $(element).attr('data-macro');
+    const args = {};
+	    
+    $.each(element.attributes, function() {
+        if (this.name.startsWith('data-args-')) {
+            args[this.name.replace('data-args-','')] = this.value;
+        }
+    });
+
+    game.macros.getName(macroName).execute(args);
+}
+
+Hooks.on("ready", () => {
+    
+	$(document).on('click', '#chat-log [data-type="macro.button"]', macro_run);
+
 	
 })
